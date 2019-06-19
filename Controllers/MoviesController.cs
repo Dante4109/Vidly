8fa8da9247fd6a017f0b8bcf57923faafbcfdfa7
RJ Zeller <rogerjohnmorellizeller@gmail.com>
2019-06-18 22:34:42 -0400

    Forked project from Mosh directly made things worse. Going back to original project

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.View_Models;
@@ -10,14 +7,30 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random 
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieVIewModel
@@ -25,7 +38,6 @@ namespace Vidly.Controllers
                 Movie = movie,
                 Customers = customers
             };
-
 
             return View(viewModel);
         }
