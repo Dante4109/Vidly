e8b73ea3287b58091d9e6515991e823b3e8717a8
RJ Zeller <rogerjohnmorellizeller@gmail.com>
2019-06-19 21:01:30 -0400

    Added customers to SQL customers table

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -2,14 +2,26 @@
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
-
+using System.Data.Entity;
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
