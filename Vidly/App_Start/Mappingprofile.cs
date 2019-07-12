86ca907ef5d0f26255b5129754ea4896a4091305
RJ Zeller <rogerjohnmorellizeller@gmail.com>
2019-07-12 18:23:27 -0400

    Created WEBAPI and automapper

---
+++ Vidly/App_Start/Mappingprofile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.DTOS;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class Mappingprofile : Profile
+    {
+        public Mappingprofile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
